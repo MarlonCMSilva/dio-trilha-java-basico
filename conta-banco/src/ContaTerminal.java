9d8092ecca563e80db93eeb63128fd1c57c0954e
MarlonCMSilva <marlon.conceicao.0308@gmail.com>
2024-09-02 19:57:44 -0300

    incluindo desafio

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args)  {
+
+    
+    Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+    System.out.println("Digite seu Nome: ");
+    String nomeDoCliente = scanner.next();
+
+    System.out.println("Digite sua agência: ");
+    String agencia = scanner.next();
+
+    System.out.println("Digite sua Conta: ");
+    int conta = scanner.nextInt();
+
+    System.out.println("Saldo: ");
+    double saldo = scanner.nextDouble();
+
+    System.out.println("Olá " + nomeDoCliente + ", obrigado por criar uma conta em nosso banco ");
+    System.out.println("sua agência é " + agencia + ", " + "conta " + conta);
+    System.out.println("seu Saldo é " + saldo +  " esta pronto para o saque.");
+
     }
 }
